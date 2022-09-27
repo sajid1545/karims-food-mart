8d4ca57ab8b23080f6cdf4df42e8247bf1c41cfb
sajid9962 <clashking1545@gmail.com>
2022-09-27 22:43:21 +0600

    header made responsive

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div className='head'>
             <h1 className='head-title'>Karims Food Mart</h1>
-            <div>
+            <div className='lists'>
                 <a href="/home">Home</a>
                 <a href="/food">Food</a>
                 <a href="/categories">Categories</a>
